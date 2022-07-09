955a6cad6a54295c3e2cf243e173827a7d1055d3
NovPas <46532077+NovPas@users.noreply.github.com>
2022-07-09 13:24:51 +0300

    Update PeteTheBaker.py

--- PeteTheBaker.py
+++ PeteTheBaker.py
@@ -26,3 +26,5 @@ recipe = {"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}
 available = {"sugar": 500, "flour": 2000, "milk": 2000}
 
 print(cakes(recipe, available))
+
+# add for test git GITHUB
